3f0846a20b2b34ea3394782a29699b4df1eed261
Adam Choragwicki <adamchor1992@gmail.com>
2022-12-07 12:54:24 +0100

    Cleanup repository

---
+++ tests/src/minefield_test.cpp
@@ -0,0 +1,32 @@
+#include "common_test_fixture.h"
+#include "minefield.h"
+#include "field.h"
+
+class MinefieldTest : public CommonTestFixture
+{
+
+};
+
+TEST_F(MinefieldTest, MinefieldSize1)
+{
+    MinefieldParameters minefieldParameters(10, 10, 10);
+    Minefield minefield(minefieldParameters);
+
+    EXPECT_EQ(minefield.getCoordinatesToFieldsMapping().size(), 100);
+}
+
+TEST_F(MinefieldTest, MinefieldSize2)
+{
+    MinefieldParameters minefieldParameters(13, 21, 20);
+    Minefield minefield(minefieldParameters);
+
+    EXPECT_EQ(minefield.getCoordinatesToFieldsMapping().size(), 273);
+}
+
+TEST_F(MinefieldTest, MinefieldSize3)
+{
+    MinefieldParameters minefieldParameters(30, 30, 10);
+    Minefield minefield(minefieldParameters);
+
+    EXPECT_EQ(minefield.getCoordinatesToFieldsMapping().size(), 900);
+}
