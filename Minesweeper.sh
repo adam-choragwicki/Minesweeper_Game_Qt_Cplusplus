923145f9eb2cba8fcda38ad5b35b51317b591b45
Adam Choragwicki <adam.choragwicki.dev@gmail.com>
2024-03-20 11:20:06 +0100

    Update README.md
    Add Minesweeper.bat

--- Minesweeper.sh
+++ Minesweeper.sh
@@ -1,8 +1,7 @@
-xhost +local:docker
-
-if [[ "$(docker images -q minesweeper 2> /dev/null)" == "" ]]; then
-  docker build -t minesweeper .
+if [ ! -d "executable_linux" ]; then
+    wget https://github.com/adam-choragwicki/Minesweeper_Game_Qt_Cplusplus/releases/latest/download/executable_linux.zip
+    unzip executable_linux.zip && rm executable_linux.zip
+    chmod +x executable_linux/Minesweeper.sh executable_linux/Minesweeper
 fi
 
-docker run -d --rm --env="DISPLAY" --net=host minesweeper
-
+./executable_linux/Minesweeper
